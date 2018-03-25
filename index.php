6a0bdfcd34b945b39b6e0f557ff02a3cb966b899
LamiaRimty <rimtycse@gmail.com>
2018-03-25 21:22:06 +0600

    just add initial files

---
+++ index.php
@@ -0,0 +1,16 @@
+
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Rimty Store</title>
+		<link rel="stylesheet" type="text/css" href="">
+	    <script src="css/bootstrap.min.css"></script>
+	    <script src="js/jquery.js"></script>
+	    <script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+	</head>
+	<body>
+	
+	</body>
+	</html>
