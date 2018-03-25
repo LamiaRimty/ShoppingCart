f080f3b6483f654715f100cea6d79fd1c634250b
LamiaRimty <rimtycse@gmail.com>
2018-03-25 23:50:00 +0600

    just added index,main files

--- index.php
+++ index.php
@@ -4,13 +4,48 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Rimty Store</title>
-		<link rel="stylesheet" type="text/css" href="">
-	    <script src="css/bootstrap.min.css"></script>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	    
 	    <script src="js/jquery.js"></script>
-	    <script src="js/bootstrap.min.js"></script>
+	      <script src="js/bootstrap.min.js"></script>
 		<script src="main.js"></script>
 	</head>
 	<body>
 	
+		<div class="
+		navbar navbar-dark bg-dark navbar-fixed-top
+		">
+<div class="container-fluid">
+	<div class="navbar navbar-header">
+		<a href="#" class="navbar-brand">Rimty Store</a>
+	</div>
+
+	<ul class="nav navbar-nav">
+		<li><a href="#"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+		<li><a href="#"><span class="glyphicon glyphicon-model-window "></span>Product</a></li>
+		<li style="width: 300px; left: 10px; top: 10px;"><input type="text" class="form-control" id="search"></li>
+
+		<li style="width: 300px; left: 10px; top: 20px;"><input type="submit" class="btn btn-primary" id="search_btn"></li>
+		
+	</ul> 
+
+
+
+	<ul class="nav navbar-nav navbar-right">
+		<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
+		
+		<li><a href="#"><span class="glyphicon glyphicon-user"></span>SignIn</a></li>
+
+		<li><a href="#"><span class="glyphicon glyphicon-user"></span>SignUp</a></li>
+
+		
+		
+	 	
+	</ul>
+
+</div>
+</div>
+
+
 	</body>
 	</html>
