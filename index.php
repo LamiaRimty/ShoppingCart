8a934aac8707dee08efd18744d93ac7741422c40
LamiaRimty <rimtycse@gmail.com>
2018-03-28 07:26:39 +0600

    added catagories structure

--- index.php
+++ index.php
@@ -10,8 +10,76 @@
  		</head>
 <body>
 <div class="navbar navbar-inverse navbar-fixed-top">
-	
+	<div class="container-fluid">
+		
+		<div class="navbar-header">
+			<a href="#" class="navbar-brand">Rimty Store</a>
+			
+		</div>
+
+		<ul class="nav navbar-nav">
+		<li><a href="#"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+
+		<li><a href="#"><span class="glyphicon glyphicon-model-window"></span>Product</a></li>
+
+
+		<li style="width: 300px; left: 10px; top: 10px;"><input type="text" class="form-control "  id="search"></li>
+
+		<li style=" left: 20px; top: 10px;"><input type="submit" class="btn btn-primary "  id="search-btn"></li>
+
+		</ul>  
+
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
+
+			<li><a href="#"><span class="glyphicon glyphicon-user"></span>SignIn</a></li>
+
+			<li><a href="#"><span class="glyphicon glyphicon-user"></span>SignUp</a></li>
+		</ul>
+	</div>
 
 </div>
+<p><br/></p>
+<p><br/></p>
+<p><br/></p>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-1"></div>
+		<div class="col-md-2">
+			<div class="nav nav-pills nav-stackeed">
+				<li class="active"><a href="#"><h4>Catagories</h4></a></li>
+				<li><a href="#">Catagories</a></li>
+				<li><a href="#">Catagories</a></li>
+				<li><a href="#">Catagories</a></li>
+				<li><a href="#">Catagories</a></li>
+			</div>
+
+			<div class="nav nav-pills nav-stackeed">
+				<li class="active"><a href="#"><h4>Brand</h4></a></li>
+				<li><a href="#">Catagories</a></li>
+				<li><a href="#">Catagories</a></li>
+				<li><a href="#">Catagories</a></li>
+				<li><a href="#">Catagories</a></li>
+			</div>
+</div>
+<div class="col-md-8">
+	<div class="col-md-1">
+	
+
+
+		
+
+
+
+
+
+
+
+
+
+
+	</div>
+</div>
+
 </body>
 </html>
