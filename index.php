28522015278ec6325fafaff4df4c70af81bc1f58
LamiaRimty <rimtycse@gmail.com>
2018-04-05 18:24:35 +0600

    jquery version changed

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
  		<title>Rimty Store</title>
 		<link rel="stylesheet"  href="css/bootstrap.min.css">    
- 	   <script src="js/jquery.js"></script>
+ 	   <script src="js/jquery2.js"></script>
 	    <script src="js/bootstrap.min.js"></script>
 	    <script src="main.js"></script>
  		</head>
